f693623b073845cd9d939d37bc2e741b8a25e6d9
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2019-05-19 00:01:43 +0530

    Update CLETAB.cpp

--- CCDSAP/Greedy Algorithms/CLETAB.cpp
+++ CCDSAP/Greedy Algorithms/CLETAB.cpp
@@ -2,29 +2,31 @@
 #include<bits/stdc++.h>
 #define FastIO ios_base::sync_with_stdio(false);    cin.tie(false);
 #define test int t; cin>>t; while(t--)
-#define loop(i,a,b) for(int i=a;i<b;++i)
+#define loop(i,a,b) for(ll i=a;i<b;++i)
 #define MOD 1000000007
-#define lo long
 #define ll long long
 using namespace std;
 
 int main(){
-    test{
-        int count=1;
-        short N, M;
-        cin>>N>>M;
-        short meal[M];
-        vector <short> C;
-        loop(i,0,M){
-            cin>>meal[i];
-            if(!binary_search(C.begin(),C.end(),meal[i]))   C.push_back(meal[i]);
+ test{
+        int n,m,clean=0;
+        cin>>n>>m;
+        int c[m];
+        map<int, pair <int,int> > mp;
+        vector <int> customer;
+        loop(i,0,m){
+            cin>>c[i];
+            if(mp.find(c[i])==mp.end())
+                mp.insert({c[i],make_pair(c[i],1)});
+            else
+                mp[c[i]].second++;
         }
-        if(N>=C.size())     cout<<C.size()<<"\n";
-        else{
-            loop(i,0,M){
-                
-            }
+        
+       for(auto itr=mp.begin();itr!=mp.end();++itr){
+            cout<<itr->first<<"\tPair: "<<itr->second.first<<" "<<itr->second.second;
+            cout<<endl;
         }
+        cout<<endl;
     }
-    return 0;
+return 0;
 }
