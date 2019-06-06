454c6b5bd7d82fe7ca6a89e371cdfe7baeb8439b
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2019-06-06 23:09:54 +0530

    AC
    Used Lamba function to sort vector.

--- CCDSAP/Greedy Algorithms/TADELIVE.cpp
+++ CCDSAP/Greedy Algorithms/TADELIVE.cpp
@@ -9,31 +9,32 @@
 using namespace std;
 
 int main(){
-    lo n,x,y,maxdiff,cur=0;
+    lo n,x,y,maxdiff,cur=0,diff;
     ll ans=0;
     cin>>n>>x>>y;
-    lo a[n],b[n],diff_a[n]={0},diff_b[n]={0};
+    lo a[n],b[n];
     bool pos[n]={false};
+    vector<pair <lo,lo> > positive_a;
+    vector<pair <lo,lo> > positive_b; 
     loop(i,0,n)     cin>>a[i];
     loop(i,0,n)     cin>>b[i];
     loop(i,0,n){
-        diff_a[i]=a[i]-b[i];
-        diff_b[i]=-1*diff_a[i];
+        diff = a[i]-b[i];
+        if(diff>=0)  positive_a.push_back(make_pair(i,diff));
+        else         positive_b.push_back(make_pair(i,-1*diff));
     }
+    sort(positive_a.begin(),positive_a.end(),[](const pair <lo,lo> p1, const pair <lo,lo> p2){
+        return p1.second > p2.second;
+    });
+    sort(positive_b.begin(),positive_b.end(),[](const pair <lo,lo> p1, const pair <lo,lo> p2){
+        return p1.second > p2.second;
+    });
     if(x<=y){
-        while(x!=0){
-            maxdiff=diff_a[cur];
-            loop(i,0,n){
-                if(diff_a[i]>maxdiff&&pos[i]==false){
-                    maxdiff=diff_a[i];
-                    cur=i;
-                }
-            }
-            if(maxdiff>=0){
+        while(x!=0 && !positive_a.empty()){
+            cur = positive_a.begin()->first;
                 pos[cur]=true;
-                diff_a[cur]=-9999999;
                 x--;
-            }
+                positive_a.erase(positive_a.begin());
         }
         loop(i,0,n){
             if(pos[i])  ans+=a[i];
@@ -41,19 +42,11 @@ int main(){
         }
     }
     else{
-        while(y!=0){
-            maxdiff=diff_b[cur];
-            loop(i,0,n){
-                if(diff_b[i]>maxdiff&&pos[i]==false){
-                    maxdiff=diff_b[i];
-                    cur=i;
-                }
-            }
-            if(maxdiff>=0){
-                pos[cur]=true;
-                diff_b[cur]=-9999999;
-                y--;
-            }
+        while(y!=0 && !positive_b.empty()){
+            cur = positive_b.begin()->first;
+            pos[cur] = true;
+            y--;
+            positive_b.erase(positive_b.begin());
         }
         loop(i,0,n){
             if(pos[i])  ans+=b[i];
