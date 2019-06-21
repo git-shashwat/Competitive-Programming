c9a00d87dc431947df0bccc720f248d5511307dd
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2019-06-21 19:00:18 +0530

    AC

--- CodeForces/568/Problem B.cpp
+++ CodeForces/568/Problem B.cpp
@@ -1,45 +1,50 @@
 //http://codeforces.com/contest/1185/problem/B
 #include<bits/stdc++.h>
 #define FastIO ios_base::sync_with_stdio(false);    cin.tie(false);
-#define test int t; cin>>t; while(t--)
+#define test int n; cin>>n; while(n--)
 #define loop(i,a,b) for(int i=a;i<b;++i)
 #define MOD 1000000007
 #define lo long
-#define ll long long
-#define push_back pb
-#define pop_back po
+#define ull unsigned long long
+#define pb push_back
+#define po pop_back
 using namespace std;
-
-/*Definition of comparePos*/
-bool comparePos(map <char,int> spos, map <char,int> tpos){
-    if (spos != tpos)   return false;
-    auto itr1 = spos.begin(), itr2 = tpos.begin();
-    for(;itr1 !=spos.end(),itr2 != tpos.end(); itr1++, itr2++){
-        int a = itr1->second, b = itr2->second;
-        if(abs(a-b) > 1)
-            return false;
-    }
-    return true;
-}
 
 int main(){
     test{
         string s,t;
         cin>>s;
         cin>>t;
-        map <char,int> smap,tmap,spos,tpos;
-        loop(i,0,s.length()){
-            smap[s[i]]++;
-            if(spos[s[i]] == 0) spos[s[i]] = i;
-        }
-        loop(i,0,t.length()){
-            tmap[t[i]]++;
-            if(tpos[t[i]] == 0) tpos[t[i]] = i;
-        }
-        if (comparePos(spos,tpos) && (tmap >= smap)){
+        if(s.length() > t.length())
+            cout<<"NO\n";
+        else{
+            int i = 0, j= 0, flag =0;
+            while(i<s.length() || j<t.length()){
+                int c1 = 0, c2 = 0;
+                char x = s[i],y = t[j];
+                if(x != y){
+                    cout<<"NO\n";
+                    flag = 1;
+                    break;
+                }
+                while(i<s.length() && s[i] == x){
+                    c1++;
+                    i++;
+                }
+                while(j<t.length() && t[j] == y){
+                    c2++;
+                    j++;
+                }
+                if(c1 > c2){
+                    cout<<"NO\n";
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0){
             cout<<"YES\n";
         }
-        else    cout<<"NO\n";
+        }
     }
     return 0;
 }
