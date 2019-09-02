804c5626a9c38f48d93b055eb9c3ed93bd7e03ec
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2019-09-02 20:08:11 +0530

    [for Macro Changed]

--- Template/temp.cpp
+++ Template/temp.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 #define FastIO ios_base::sync_with_stdio(0);    cin.tie(0);   cout.tie(0);
 #define test int t; cin>>t; while(t--)
-#define loop(i,a,b) for(int i=a;i<b;++i)
+#define loop(a,b) for(int i=a;i<b;++i)
 #define loop0(b) for(int i=0;i<b;++i)
 #define MOD 1000000007
 #define ull unsigned long long
