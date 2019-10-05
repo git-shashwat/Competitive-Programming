70ee874065b23736f4d404678fb9a2328334c62f
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2019-10-05 22:22:40 +0530

    [AC]

---
+++ CCDSAP/Binary Search/STACKS.cpp
@@ -0,0 +1,37 @@
+//https://www.codechef.com/problems/STACKS
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(0);    cin.tie(0);   cout.tie(0);
+#define test int t; cin>>t; while(t--)
+#define loop(a,b) for(int i=a;i<b;++i)
+#define loop0(b) for(int i=0;i<b;++i)
+#define MOD 1000000007
+#define ull unsigned long long
+#define ll long long
+#define pb push_back
+#define po pop_back
+
+using namespace std;
+
+int main(){
+    test {
+        int n;
+        cin>>n;
+        ull a[n];
+        vector <ull> v;
+        loop0(n) {
+            cin>>a[i];
+            auto itr = upper_bound(v.begin(), v.end(), a[i]);
+            if (itr == v.end()) {
+                v.pb(a[i]);
+            } else {
+                *itr = a[i];
+            }
+        }
+        cout<<v.size()<<" ";
+        for (auto i:v) {
+            cout<<i<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
