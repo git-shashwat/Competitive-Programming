9e4c185ad13dea84d14aded093ff380f9395c4a3
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2020-05-23 13:35:00 +0530

    [AC]
    step #10

--- codechef/ds learning series/week 2/STFOOD.cpp
+++ codechef/ds learning series/week 2/STFOOD.cpp
@@ -1,4 +1,4 @@
-//https://www.codechef.com/LRNDSA01/problems/LADDU
+//https://www.codechef.com/LRNDSA02/problems/STFOOD
 #include<bits/stdc++.h>
 #define FastIO ios_base::sync_with_stdio(0);    cin.tie(0);   cout.tie(0);
 #define test int t; cin>>t; while(t--)
@@ -15,25 +15,17 @@ using namespace std;
 int main(){
     FastIO
     test {
-        int d0, d1, d2, d3, a[4];
-        ull k, sum = 0, patternCount = 0, rem = 0;
-        cin>>k>>d0>>d1;
-        sum = d0 + d1;
-        d2 = (d0 + d1)%10;
-        sum += d2;
-        d3 = sum%10;
-        rem = (k-3)%4;
-        a[0] = d3;
-        a[1] = (a[0]*2)%10;
-        a[2] = (a[1]*2)%10;
-        a[3] = (a[2]*2)%10;
-        patternCount = (k-3)/4;
-        loop0(rem) {
-            sum += a[i];
+        int n;
+        cin>>n;
+        int s[n], v[n], p[n], maxProfit = 0;
+        loop0(n) {
+            cin>>s[i]>>p[i]>>v[i];
         }
-        sum += patternCount*(a[0]+a[1]+a[2]+a[3]);
-        if (k == 2) sum = d0+d1;
-        if (k == 3) sum = d0+d1+d2;
-        !(sum%3) ? cout<<"YES\n" : cout<<"NO\n";
+        loop0(n) {
+            int sharePerPerson = floor(p[i]/(s[i]+1));
+            maxProfit = max(maxProfit, sharePerPerson*v[i]);
+        }
+        cout<<maxProfit<<"\n";
     }   
+    return 0;
 }
