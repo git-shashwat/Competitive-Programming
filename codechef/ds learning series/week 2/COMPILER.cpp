1d540cb42591d789576a7b8402444855952386f9
Shashwat Tyagi <43851597+git-shashwat@users.noreply.github.com>
2020-05-26 20:35:54 +0530

    [AC]
    step #12

---
+++ codechef/ds learning series/week 2/COMPILER.cpp
@@ -0,0 +1,32 @@
+ //https://www.codechef.com/LRNDSA02/problems/COMPILER
+#include<bits/stdc++.h>
+#define FastIO ios_base::sync_with_stdio(0);    cin.tie(0);   cout.tie(0);
+#define test int t; cin>>t; while(t--)
+#define loop(a,b) for(int i=a;i<b;++i)
+#define loop0(b) for(int i=0;i<b;++i)
+#define MOD 1000000007
+#define ull unsigned long long
+#define ll long long
+#define pb push_back 
+#define po pop_back 
+
+using namespace std;
+
+int main(){
+    test {
+        int ans = 0, t = 0;
+        string xml;
+        cin>>xml;
+        loop0(xml.size()) {
+            if (xml[i] == '<') {
+                t++;
+            } else {
+                t--;
+                if (t == 0)     ans = max(ans,i+1);
+                else if (t < 0) break;
+            }
+    }
+    cout<<ans<<"\n";
+    }
+    return 0;
+}
